53dccefa9865b74be8c7d66cefbe741e00f4941d
amritrajputt <vishal179singh@gmail.com>
2025-12-10 00:11:09 +0530

    middleware for session

---
+++ backend/middleware/usermiddleware.js
@@ -0,0 +1,11 @@
+const userMiddleware = (req, res, next) => {
+    if (!req.session || !req.session.userId) {
+        return res.status(401).json({
+            message: "User not authenticated"
+        });
+    }
+
+    next();
+};
+
+module.exports = { userMiddleware };
